0211e3f41db047c0deaba861624246df948e0b04
Hoshino <2453490867@qq.com>
2025-05-22 22:16:02 +0800

    day3 --导入地址簿模块功能实现 --用户下单功能实现 --订单支付功能实现 --订单状态定时处理（Spring Task）（涉及订单自动取消以及长时间在配送单的自动取消） --来单提醒以及客户催单功能的实现（WebSocket）

--- server/src/main/java/com/delivery/mapper/UserMapper.java
+++ server/src/main/java/com/delivery/mapper/UserMapper.java
@@ -20,4 +20,12 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+    /**
+     * 通过用户id获取用户对象
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User getById(Long userId);
 }
---
+++ server/src/main/java/com/delivery/service/AddressBookService.java
@@ -0,0 +1,20 @@
+package com.delivery.service;
+
+import com.delivery.entity.AddressBook;
+import java.util.List;
+
+public interface AddressBookService {
+
+    List<AddressBook> list(AddressBook addressBook);
+
+    void save(AddressBook addressBook);
+
+    AddressBook getById(Long id);
+
+    void update(AddressBook addressBook);
+
+    void setDefault(AddressBook addressBook);
+
+    void deleteById(Long id);
+
+}
